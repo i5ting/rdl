60000cc460538018be34137d40f2557ee6aae780
i5ting <i5ting@126.com>
2016-12-12 10:21:43 +0800

    refact, please ignore

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-var fs = require('fs');
-var path = require('path');
+var fs = require('fs')
+var path = require('path')
 
-var list = [];
-var count = 0;
+var list = []
+var count = 0
 
-module.exports = function readDir(_path, callback) {
+var rdl = function (dir, cb) {
+  count++
+  fs.readdir(dir, function (err, files) {
+    if (err) {
+      console.log(err)
+      return
+    }
+    files.forEach(function (file, i) {
+      var stat = fs.lstatSync(path.join(dir, file))
+      if (stat.isFile()) {
+          list.push(path.join(dir, file))
+      } else if (stat.isDirectory()) {
+          rdl(path.join(dir, file), cb)
+      }
+
+      if ((i + 1) === files.length) {
+          count--
+      }
+
+      if(count === 0){
+          cb(list)
+      }
+    })
+  })
+}
+
+module.exports = function(dir, cb) {
   if (count === 0){
-    list = [];
+    list = []
   }
-    var toExec = function (_path) {
-        count++;
-        fs.readdir(_path, function (err, files) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            files.forEach(function (file, i) {
-                var stat = fs.lstatSync(path.join(_path, file));
-                if (stat.isFile()) {
-                    list.push(path.join(_path, file));
-                } else if (stat.isDirectory()) {
-                    toExec(path.join(_path, file));
-                }
-
-                if ((i + 1) === files.length) {
-                    count--;
-                }
-
-                if(count === 0){
-                    callback(list);
-                }
-            });
-        });
-    };
-
-    toExec(_path);
-};
-
+    
+  rdl(dir, cb)
+}
